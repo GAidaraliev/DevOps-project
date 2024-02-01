2deb3115c477f333103df24b5c56fe041ac7f3ff
GAidaraliev <linchesel@gmail.com>
2024-02-01 20:34:37 +0500

    modified CI/CD

--- user-api/src/dbClient.js
+++ user-api/src/dbClient.js
@@ -4,7 +4,7 @@ const configure = require('./configure')
 const config = configure()
 var db = redis.createClient({
   host: process.env.REDIS_HOST || config.redis.host,
-  port: process.env.REDIS_PORT || config.redis.port || '3000',
+  port: process.env.REDIS_PORT || config.redis.port,
  
   retry_strategy: () => {
     return new Error("Retry time exhausted")
